a6b80b3ca91a75c7add8ac44136f3235a7e9fc23
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-06-18 23:05:34 +0800

    I like this version better.

--- robot-name/robot_name.clj
+++ robot-name/robot_name.clj
@@ -1,15 +1,19 @@
 (ns robot-name)
 
 (defn rand-alpha []
-   (rand-nth (into [] (map char (range 65 91)))))
+  (rand-nth (into [] (map char (range 65 91)))))
+
+(defn rand-name []
+  (apply str
+         (concat
+          (repeatedly 2 #(rand-alpha))
+          (repeatedly 3 #(rand-int 10)))))
 
 (defn robot []
-  (apply str
-    (concat
-      (repeatedly 2 #(rand-alpha))
-      (repeatedly 3 #(rand-int 10)))))
+  (atom (rand-name)))
 
-(defn robot-name [name] name)
+(defn robot-name [name]
+  @name)
 
 (defn reset-name [name]
-  (alter-var-root #'name robot))
+  (reset! name (rand-name)))
